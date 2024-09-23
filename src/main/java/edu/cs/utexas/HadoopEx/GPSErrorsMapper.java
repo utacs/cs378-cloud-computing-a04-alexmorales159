74c406ea6ab97332154d32332c7b5906829068e5
amorales159 <alexandromorales159@gmail.com>
2024-09-23 12:59:34 -0500

    invalid lines check

--- src/main/java/edu/cs/utexas/HadoopEx/GPSErrorsMapper.java
+++ src/main/java/edu/cs/utexas/HadoopEx/GPSErrorsMapper.java
@@ -13,23 +13,44 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 /*
- *  Output:     (hour of day, number of errors)
+ *  Output:     (hour of day, 1)
+ *  Hour needs to be between 1-24
  */
 public class GPSErrorsMapper extends Mapper<Text, Text, IntWritable, IntWritable> {
     // parser to convert string to Date (tp grab hour)
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    static private int NUM_ATTR = 17;
+    static private float SMALL_ERROR = 0.001f;
 
 	public void map(Object key, Text value, Context context)
     throws IOException, InterruptedException {
-        String[] myVal = value.toString().split(",");
+        String[] attr = value.toString().split(",");
 
-        String departure = myVal[2];
-        int a_longitude = Integer.parseInt(myVal[6]);
-        int a_latitude = Integer.parseInt(myVal[7]);
-        int b_longitude = Integer.parseInt(myVal[8]);
-        int b_latitude = Integer.parseInt(myVal[9]);
+        boolean invalid = false;    // only add to GPS errors if rest of line is valid
 
-        if (a_longitude == 0 || a_latitude == 0 || b_longitude == 0 || b_latitude == 0) {
+        String departure = attr[2];
+        int a_longitude = Integer.parseInt(attr[6]);
+        int a_latitude = Integer.parseInt(attr[7]);
+        int b_longitude = Integer.parseInt(attr[8]);
+        int b_latitude = Integer.parseInt(attr[9]);
+
+        // correct num commas
+        if (attr.length != NUM_ATTR) {
+            invalid = true;
+        }
+
+        // all money values are float and valid
+        try {
+            Float calc_total = Float.parseFloat(attr[11]) + Float.parseFloat(attr[12]) + Float.parseFloat(attr[13]) + Float.parseFloat(attr[14]) + Float.parseFloat(attr[15]);
+            Integer total_time = Integer.parseInt(attr[4]);
+            if (Math.abs(calc_total - Float.parseFloat(attr[16])) > SMALL_ERROR || calc_total - 500.0 > SMALL_ERROR || total_time == 0) {
+                invalid = true;
+            }
+        } catch (NumberFormatException e) {
+            invalid = true;
+        }
+
+        if (!invalid && (a_longitude == 0 || a_latitude == 0 || b_longitude == 0 || b_latitude == 0)) {
             // GPS error
             try {
                 LocalDateTime dateTime = LocalDateTime.parse(departure, formatter);

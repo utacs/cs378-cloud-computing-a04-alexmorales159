909d8f6a450fafdd53a8bb7356f2602dd9cbb6b5
amorales159 <alexandromorales159@gmail.com>
2024-09-23 13:15:13 -0500

    reducer for errors/made job

---
+++ src/main/java/edu/cs/utexas/HadoopEx/GPSErrorsReducer.java
@@ -0,0 +1,20 @@
+package edu.cs.utexas.HadoopEx;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class GPSErrorsReducer extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+    public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) 
+            throws IOException, InterruptedException {
+        
+        int total = 0;
+
+        for (IntWritable value : values) {
+            total += value.get();
+        }
+
+        context.write(key, new IntWritable(total));
+    }
+}
